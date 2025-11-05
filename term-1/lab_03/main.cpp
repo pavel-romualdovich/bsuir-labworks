e462ba89262444c804a7d73aa6931046ce6f115f
Pavel Cidik <pavel.romualdovich@gmail.com>
2025-11-05 17:01:22 +0300

    labwork 3: change table output

--- term-1/lab_03/main.cpp
+++ term-1/lab_03/main.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 double EPS = 10e-15;
+int TABLE_ROWS = 10;
 
 int main () {
     using namespace std;
@@ -24,22 +25,35 @@ int main () {
     }
 
     int row_size = 140;
-    double step = (right_border - left_border) / 10;
+    double step = (right_border - left_border) / TABLE_ROWS;
 
-    cout << "============================================" << endl;
-    cout << "Таблица значений xcos(x) и ряда Тейлора s(x)" << endl;
-    cout << "============================================" << endl;
-    cout << "     x     |     xcos(x)     |     s(x)     " << endl;
-    cout << "--------------------------------------------" << endl;
+    cout << "\n";
+    cout << "╔══════════════════════════════════════════════╗" << endl;
+    cout << "║ Таблица значений xcos(x) и ряда Тейлора s(x) ║" << endl;
+    cout << "╠══════════════╦═════════════════╦═════════════╣" << endl;
+    cout << "║      x       ║     xcos(x)     ║     s(x)    ║" << endl;
+    cout << "╠══════════════╬═════════════════╬═════════════╣" << endl;
 
-    for (int i = 0; i < 11; i++) {
-        double x = left_border + i * step;
+    for (int i = 0; i < TABLE_ROWS; i++) {
+        double x = left_border + step * i;
         double x_cosx = x * cos(x);
 
-        abs(x) < EPS? cout << 0: cout << x; // Проверка на число, близкое к нулю
-        cout << "     |     " << setw(10);
-        abs(x_cosx) < EPS? cout << 0: cout << x_cosx; // Проверка на число, близкое к нулю
-        cout << "     |     " << setw(10);
+        cout << "║ ";
+
+        if (abs(x) < EPS) {
+            cout << setw(12) << "0";
+        } else {
+            cout << setw(12) << fixed << setprecision(6) << x;
+        }
+        cout << " ║ ";
+
+        // Вывод x*cos(x)
+        if (abs(x_cosx) < EPS) {
+            cout << setw(15) << "0";
+        } else {
+            cout << setw(15) << fixed << setprecision(6) << x_cosx;
+        }
+        cout << " ║ ";
 
         double taylor_value = x;
         double tmp = x;
@@ -50,9 +64,17 @@ int main () {
             taylor_value += tmp;
         }
 
-        abs(taylor_value) < EPS? cout << 0: cout << taylor_value; // Проверка на число, близкое к нулю
-        cout << endl;
+        // Вывод ряда Тейлора
+        if (abs(taylor_value) < EPS) {
+            cout << setw(11) << "0";
+        } else {
+            cout << setw(11) << fixed << setprecision(6) << taylor_value;
+        }
+        cout << " ║" << endl;
     }
+
+    cout << "╚══════════════╩═════════════════╩═════════════╝" << endl;
+    cout << endl;
 
     return 0;
 }
